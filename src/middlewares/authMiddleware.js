04d6cde54586dbade9a89b6f59badea236592578
mahesmahfud <mahesmahfud@gmail.com>
2025-11-11 09:57:22 +0700

    fix: Remove rogue 'export' keyword in CJS middleware

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -48,7 +48,7 @@ const isMurid = (req, res, next) => {
   }
 };
 
-export const isAdmin = (req, res, next) => {
+const isAdmin = (req, res, next) => {
   if (req.user && req.user.role === 'admin') {
     next();
   } else {
